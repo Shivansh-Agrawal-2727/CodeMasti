a9defeb9a82e51e71a0c5c43facaea4cf0ed8868
Shivansh Agrawal <agrawal.shivansh2004@gmail.com>
2026-01-21 11:18:21 +0530

    changes in login and hero section

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -8,19 +8,19 @@ const Hero = () => {
     <div className='px-4 md:px-0 '>
       <div className='max-w-7xl mx-auto flex flex-col md:flex-row items-center h-[600px] my-10 md:my-0'>
         {/* text section */}
-        <div className="max-w-2xl">
-        <h1 className="text-4xl md:text-6xl font-bold mb-4 ">Explore the Latest Tech & Web Trends</h1>
-        <p className="text-lg md:text-xl opacity-80 mb-6 ">
-          Stay ahead with in-depth articles, tutorials, and insights on web development, digital marketing, and tech innovations.
-        </p>
-        <div className="flex space-x-4">
-          <Link to={"/dashboard/write-blog"}><Button className="text-lg ">Get Started</Button></Link>
-          <Link to={"/about"}><Button variant="outline" className="border-white px-6 py-3 text-lg">Learn More</Button></Link>
+        <div className="max-w-2xl pl-4 md:pl-8">
+          <h1 className="text-4xl md:text-6xl font-bold mb-4 ">Explore the Latest Tech & Web Trends</h1>
+          <p className="text-lg md:text-xl opacity-80 mb-6 ">
+            Stay ahead with in-depth articles, tutorials, and insights on web development, digital marketing, and tech innovations.
+          </p>
+          <div className="flex space-x-4">
+            <Link to={"/dashboard/write-blog"}><Button className="text-lg ">Get Started</Button></Link>
+            <Link to={"/about"}><Button variant="outline" className="border-white px-6 py-3 text-lg">Learn More</Button></Link>
+          </div>
         </div>
-      </div>
         {/* image section */}
         <div className=' flex items-center justify-center '>
-            <img src={heroImg} alt="" className='md:h-[550px] md:w-[550px]'/>
+          <img src={heroImg} alt="" className='md:h-[550px] md:w-[550px]' />
         </div>
       </div>
     </div>
